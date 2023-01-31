fcc22ab34020f137676c0962036da214d3fe24a2
Shayam2 <shayamsoni2002@gmail.com>
2023-01-31 15:46:02 +0530

    first commit

---
+++ About.php
@@ -0,0 +1,90 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6">
+					<img src="img/dev1.png" alt="showcase image">
+				</div>
+				<div class="col-md-6">
+					<div class="about-text">
+						<h3>Job Portal</h3>
+						<p></p>
+						 <p>The purpose of our website is to ease the existing systems by mechanized procedures and completely fulfilling our client's requests. We aim to automate and continuously expand our horizons to make our website accessible to all. <p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					<div class="about">
+				
+						
+						<div class="row">
+							<div class="col-md-4">
+								
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						<div class="block-heading-six">
+							<h4 class="bg-color">Our Team</h4>
+						</div>
+						<br>
+						
+						<!-- Our team starts -->
+						
+						<div class="team-six">
+							<div class="row">
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team1.jpg" alt="">
+										<!-- Name -->
+										<h4>Ashutosh</h4>
+										<span class="deg">MEMBER</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team2.jpg" alt="">
+										<!-- Name -->
+										<h4>Aditya</h4>
+										<span class="deg">MEMBER</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team3.jpg" alt="">
+										<!-- Name -->
+										<h4>Onkar</h4>
+										<span class="deg">MEMBER</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team4.jpg" alt="">
+										<!-- Name -->
+										<h4>Shason</h4>
+										<span class="deg">MEMBER</span> 
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
